2a1351a45569d86a2dc1a9ebae3d68bed46fe823
101018034 <naseeba.shegiwal@dcmail.ca>
2026-07-17 13:36:10 -0700

    Complete silly story JavaScript

--- Assignment4/Part1/script.js
+++ Assignment4/Part1/script.js
@@ -7,6 +7,7 @@ const story = document.querySelector(".story");
 function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
+
 }
 
 // Raw text strings
@@ -24,9 +25,10 @@ const events = [
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-  return storyText;
+    const randomCharacter = randomValueFromArray(characters);
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
+let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;  return storyText;
 }
 
 // Event listener and partial generate function definition
@@ -34,16 +36,18 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+    let newStory = returnRandomStoryString();
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
-  }
-
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  const weight = `${Math.round(300 / 14)} stone`;
+const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+newStory = newStory.replace("94 Fahrenheit", temperature);
+newStory = newStory.replace("300 pounds", weight);
+    }
+story.textContent = newStory;
   story.style.visibility = "visible";
 }
